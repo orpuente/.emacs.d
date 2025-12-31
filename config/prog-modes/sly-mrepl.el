269caaa14ab7da699b6bde861b199f80e168b128
Oscar Puente <orpuente98@gmail.com>
2025-12-31 11:09:22 -0800

    add 7-billion-humans syntax highliting

--- config/prog-modes/sly-mrepl.el
+++ config/prog-modes/sly-mrepl.el
@@ -1,5 +1,5 @@
-(with-eval-after-load 'sly
-  (orpl-lisp-editing-keybinds sly-mrepl-mode-map))
+;; (with-eval-after-load 'sly
+;;  (orpl-lisp-editing-keybinds sly-mrepl-mode-map))
 
 (add-to-hook 'sly-mrepl-mode-hook
 	     '(shared-lisp-pretty-symbols))
