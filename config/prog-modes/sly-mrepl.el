ace933d91d827cd0e464ba3f5affd5ec9d33b963
Oscar Puente <orpuente98@gmail.com>
2026-05-27 10:35:57 -0700

    sly-mrepl ansi colors

--- config/prog-modes/sly-mrepl.el
+++ config/prog-modes/sly-mrepl.el
@@ -1,5 +1,9 @@
 ;; (with-eval-after-load 'sly
 ;;  (orpl-lisp-editing-keybinds sly-mrepl-mode-map))
 
+(defun enable-ansi-colors-in-sly-mrepl ()
+  (add-to-list 'sly-mrepl-output-filter-functions 'ansi-color-apply))
+
 (add-to-hook 'sly-mrepl-mode-hook
-	     '(shared-lisp-pretty-symbols))
+	     '(shared-lisp-pretty-symbols
+	       enable-ansi-colors-in-sly-mrepl))
