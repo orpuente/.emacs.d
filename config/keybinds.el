c2404a8c607675b70f241c780ca2da128cb5749e
Oscar Puente <orpuente98@gmail.com>
2026-07-17 00:43:00 -0700

    better s-exp deleting logic

--- config/keybinds.el
+++ config/keybinds.el
@@ -1,16 +1,16 @@
-(defun orpl-translate-keys (&optional frame) ; Reason:
+(defun my/translate-keys (&optional frame) ; Reason:
   (keyboard-translate ?\C-i ?\s-i)       ; C-i = TAB
   (keyboard-translate ?\C-\[ ?\s-\[))    ; C-[ = M-w 
-(add-hook 'after-make-frame-functions #'orpl-translate-keys)
-(orpl-translate-keys)
+(add-hook 'after-make-frame-functions #'my/translate-keys)
+(my/translate-keys)
 
 ;; Unbinds
 (keymap-unset lisp-interaction-mode-map "C-M-i")
 (keymap-unset emacs-lisp-mode-map "C-M-i")
 (dolist (key '("C-a" "C-n" "C-o" "C-p"
-	       "C-t" "C-]" "H-[" "M-e"
-	       "C-k" "C-l" "H-i" "C-j"
-	       "s-j" "s-l" "s-i" "s-k" "s-["))
+			   "C-t" "C-]" "H-[" "M-e"
+			   "C-k" "C-l" "H-i" "C-j"
+			   "s-j" "s-l" "s-i" "s-k" "s-["))
   (keymap-global-unset key t)
   (keymap-unset lisp-interaction-mode-map key t))
 
@@ -22,15 +22,41 @@
 (global-set-key (kbd "C-v") #'yank)
 (global-set-key (kbd "C-a") #'mark-whole-buffer)
 
-(defun orpl-backward-transpose-sexp ()
+(defun my/backward-transpose-sexp ()
   (interactive)
   (sp-transpose-sexp -1))
 
-(defun orpl-lisp-editing-keybinds (keymap)
+(defun my/kill-whitespace ()
+  (interactive)
+  (let ((start (point)))
+	(skip-chars-forward " \t\n\r\f")
+	(when (> (point) start)
+	  (delete-region start (point))
+	  t)))
+
+(defun my/kill-whitespace-backward ()
+  (interactive)
+  (let ((start (point)))
+	(skip-chars-backward " \t\n\r\f")
+	(when (< (point) start)
+	  (delete-region (point) start)
+	  t)))
+
+(defun my/kill-whitespace-or-sexp ()
+  (interactive)
+  (unless (my/kill-whitespace)
+	(sp-kill-sexp)))
+
+(defun my/kill-whitespace-or-sexp-backward ()
+  (interactive)
+  (unless (my/kill-whitespace-backward)
+	(sp-backward-kill-sexp)))
+
+(defun my/lisp-editing-keybinds (keymap)
   ;; C-M-[key] (Ctrl + Alt + [key])
-  (define-key keymap (kbd "M-<left>") #'orpl-backward-transpose-sexp)
+  (define-key keymap (kbd "M-<left>") #'my/backward-transpose-sexp)
   (define-key keymap (kbd "M-<right>") #'sp-transpose-sexp)
-  (define-key keymap (kbd "M-<up>") #'orpl-backward-transpose-sexp)
+  (define-key keymap (kbd "M-<up>") #'my/backward-transpose-sexp)
   (define-key keymap (kbd "M-<down>") #'sp-transpose-sexp)
 
   ;; C-[Arrows]
@@ -46,7 +72,10 @@
   (define-key keymap (kbd "M-]") #'sp-forward-slurp-sexp)
   (define-key keymap (kbd "C-r") #'raise-sexp)
   (define-key keymap (kbd "C-(") #'sp-backward-unwrap-sexp)
-  (define-key keymap (kbd "C-)") #'sp-unwrap-sexp))
+  (define-key keymap (kbd "C-)") #'sp-unwrap-sexp)
+  (define-key keymap (kbd "C-<delete>") #'my/kill-whitespace-or-sexp)
+  (define-key keymap (kbd "C-<backspace>") #'my/kill-whitespace-or-sexp-backward))
+
 
 ;; Quick Help
 (global-set-key (kbd "M-h") (kbd "C-h o <return>"))
--- config/prog-modes/lisp-mode.el
+++ config/prog-modes/lisp-mode.el
@@ -5,13 +5,13 @@
 ;;;; Keybinds
 (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'eval-defun)
 (define-key lisp-mode-shared-map (kbd "C-x C-p") #'sly-eval-last-sexp-in-popup-buffer)
-(orpl-lisp-editing-keybinds lisp-mode-shared-map)
+(my/lisp-editing-keybinds lisp-mode-shared-map)
 
 ;;;; Hooks
 (add-hook 'sly-mode-hook
           (lambda ()
             (unless (sly-connected-p)
-	      (define-key sly-mode-map (kbd "M-e") (lambda () (interactive) (sly-compile-defun) (sleep-for 0.1) (sly-disassemble-definition)))
+			  (define-key sly-mode-map (kbd "M-e") (lambda () (interactive) (sly-compile-defun) (sleep-for 0.1) (sly-disassemble-definition)))
               (save-excursion (sly)))))
 
 ;;;; CL3
