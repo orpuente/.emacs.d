e88e59fcff37faebaacfb385fa4991d784a40df5
Oscar Puente <orpuente98@gmail.com>
2026-07-17 00:37:54 -0700

    scheme and cl3 pretty symbols

--- config/prettify-symbols.el
+++ config/prettify-symbols.el
@@ -39,9 +39,44 @@ lisp modes. It is to be added to the lisp mode hooks."
 	  ("sqrt"         . ?√)))
   (prettify-symbols-mode 1))
 
+(defun cl3-pretty-symbols ()
+  "This function enables pretty symbols for CL3."
+  (setq prettify-symbols-alist
+	'(("lambda"       . ?λ)
+	  ("def"          . ?󰊕)
+	  ("defm"         . ?)
+	  ("defstruct"    . ?)
+	  ("defmacro"     . ?󰡷)
+	  ("defprm"       . ?)
+	  ("defvar"       . ?󰕷)
+	  ("cond"         . ?󰘬)
+	  ("self"         . ?○)
+	  ("!="           . ?≠)
+	  ("<="           . ?≤)
+	  (">="           . ?≥)
+	  ("for"          . ?∀)
+	  ("list"         . ?ℒ)
+	  ("nil"          . ?∅)))
+  (prettify-symbols-mode 1))
+
+(defun scheme-pretty-symbols ()
+  "This function enables pretty symbols for CL3."
+  (setq prettify-symbols-alist
+	'(("lambda"       . ?λ)
+	  ("define"       . ?𝒹)
+	  ("cond"         . ?󰘬)
+	  ("!="           . ?≠)
+	  ("<="           . ?≤)
+	  (">="           . ?≥)
+	  ("list"         . ?ℒ)
+	  ("null"         . ?∅)))
+  (prettify-symbols-mode 1))
+
 ;; Pretty symbols hooks.
-(add-hook 'prog-mode-hook #'prog-mode-pretty-symbols)
-(add-hook 'emacs-lisp-mode-hook #'shared-lisp-pretty-symbols)
-(add-hook 'lisp-mode-hook #'shared-lisp-pretty-symbols)
+(add-hook 'prog-mode-hook             #'prog-mode-pretty-symbols)
+(add-hook 'emacs-lisp-mode-hook       #'shared-lisp-pretty-symbols)
+(add-hook 'lisp-mode-hook             #'shared-lisp-pretty-symbols)
+(add-hook 'cl3-mode-hook              #'cl3-pretty-symbols)
+(add-hook 'scheme-mode-hook           #'scheme-pretty-symbols)
 (add-hook 'lisp-interaction-mode-hook #'shared-lisp-pretty-symbols)
 
